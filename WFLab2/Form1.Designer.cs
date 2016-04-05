7e8f461fcc5d6d1af31a40458ac04c0a035fdbdf
Саша <@ALEXANDR_PC>
2016-04-05 16:55:56 +0300

    Add project files.

---
+++ WFLab2/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace WFLab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Out = new System.Windows.Forms.Label();
+            this.Start = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Out
+            // 
+            this.Out.AutoSize = true;
+            this.Out.Location = new System.Drawing.Point(77, 47);
+            this.Out.Name = "Out";
+            this.Out.Size = new System.Drawing.Size(0, 17);
+            this.Out.TabIndex = 0;
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(289, 161);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(129, 36);
+            this.Start.TabIndex = 1;
+            this.Start.Text = "Start!";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(752, 256);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.Out);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Out;
+        private System.Windows.Forms.Button Start;
+    }
+}
+
---
+++ WFLab2/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+using System.IO;
+
+namespace WFLab2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            FileStream i_stream = File.Open("Input.txt", FileMode.Open, FileAccess.Read);
+            string input = string.Empty;
+
+            if (i_stream != null)
+            {
+                StreamReader reader = new StreamReader(i_stream);
+                input = reader.ReadToEnd();
+                i_stream.Close();
+            }
+
+            string[] text = input.Split(' ', ',', '.', '/', '!', '\n','\t');
+            List<int> mas = new List<int>();
+            List<string> words = new List<string>();
+            int res;
+
+            string label = string.Empty;
+
+            for (int i = 0; i < text.Length; ++i)
+                if (int.TryParse(text[i], out res))
+                    mas.Add(res);
+                else if (words.IndexOf(text[i]) == -1)
+                {
+                    words.Add(text[i]);
+                    label += text[i] + " ";
+                }
+                else
+                    continue;
+
+            FileStream o_stream = File.Open("Output.txt", FileMode.Open, FileAccess.Write);
+            if (o_stream != null)
+            {
+                StreamWriter writer = new StreamWriter(o_stream);
+
+                writer.Write(mas.Count);
+                writer.Flush();
+                o_stream.Close();
+            }
+
+            label+= "\n\nMax " + M(mas, 1) + "\nMin " + M(mas, -1);
+            Out.Text = label;
+        }
+
+        private static int M(List<int> mas, int l)
+        {
+            int temp = mas[0];
+
+            for (int i = 0; i < mas.Count; ++i)
+                if (l * mas[i] > temp * l)
+                    temp = mas[i];
+
+            return temp;
+        }
+    }
+}
